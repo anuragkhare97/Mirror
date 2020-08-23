3f23b31d857e694b98a9f0aa26a1149deb97d345
anurag <anuragkhare97@gmail.com>
2020-08-23 00:36:28 -0600

    fdvf

--- Code/test.py
+++ Code/test.py
@@ -17,20 +17,29 @@ def findColor(img):
     lower = np.array([0, 20, 70])
     upper = np.array([20, 255, 255])
     mask = cv2.inRange(imgHSV, lower, upper)
-    x,y=getContours(mask)
-    cv2.circle(imgResult,(x,y),10,(255,0,0),cv2.FILLED)
-    #cv2.imshow('img', mask)
+    x,y=getContours(mask,imgResult)
+    cv2.imshow('img', mask)
 
-def getContours(img):
+def getContours(img,imgContour):
     contours,hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     x,y,w,h = 0,0,0,0
     for cnt in contours:
         area = cv2.contourArea(cnt)
-        if area > 500:
+        if area > 5000:
             cv2.drawContours(imgResult, cnt, -1, (255,0,0),3)
             peri = cv2.arcLength(cnt, True)
             approx = cv2.approxPolyDP(cnt, 0.02*peri,True)
             x,y,w,h = cv2.boundingRect(approx)
+            cv2.rectangle(imgContour, (x,y),(x+w,y+h),(0,255,0),5)
+            cv2.putText(imgContour, "Points: " + str(len(approx)), (x+w + 20,y+20), cv2.FONT_HERSHEY_COMPLEX,0.7,
+                        (0,255,0),2)
+            if len(approx) == 12 or len(approx) == 13:
+                cv2.putText(imgContour, "Five Fingers", (x + w + 20, y + 70), cv2.FONT_HERSHEY_COMPLEX,
+                            0.7,
+                            (0, 255, 0), 2)
+            cv2.putText(imgContour, "Area: " + str(int(area)), (x + w + 20, y + 45), cv2.FONT_HERSHEY_COMPLEX, 0.7,
+                        (0, 255, 0), 2)
+            cv2.drawContours(imgResult, [approx], -1, (0, 0, 255), 3)
     return x+w//2,y
 
 
@@ -40,7 +49,6 @@ while True:
     #img = cv2.flip(img,1)
     kernel = np.ones((3,3),np.uint8)
     findColor(img)
-
     cv2.imshow("Result",imgResult)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
